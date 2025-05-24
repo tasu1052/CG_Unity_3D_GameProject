032bcf5d44303a55e0fba6288a534a55dcf66536
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-05-24 19:57:16 +0900

    Fix : Inventory Open and Close

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InventoryManager
+public class InventoryManager : MonoBehaviour
 {
-    void Start()
+    public RectTransform inventoryTransform;
+    private void Start()
     {
-
+        inventoryTransform.anchoredPosition = new Vector2(0, 2000); // 화면에서 안보이게하기
+    }
+    public void CloseInventory()
+    {
+        inventoryTransform.anchoredPosition = new Vector2(0, 2000); // 화면에서 안보이게하기
     }
 
+    public void OpenInventory()
+    {
+        inventoryTransform.anchoredPosition = new Vector2(0, 0); // 화면에서 보이게하기
+    }
 }
